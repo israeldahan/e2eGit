683e23f92a48d644a88c9be732ac80f49bfda982
israel david dahan <id@STUDIODVD.CO.IL>
2021-04-19 20:14:21 +0300

    add tabs

--- script1.js
+++ script1.js
@@ -2,15 +2,20 @@ let cloud = document.getElementById("cloud");
 let green = document.getElementById("green");
 let grey = document.getElementById("grey");
 let par = document.getElementById("par");
+let btn = document.getElementById("btn");
+let divDragAndDrop = document.getElementById("divDragAndDrop");
 
 cloud.addEventListener("click", el => {
     console.log("i'm cloud");
+    console.log(el);
     par.innerHTML = "i'm cloud";
     el.stopPropagation();
+    el.preventDefault();
 });
 
 grey.addEventListener("click", el => {
     console.log("i'm grey");
+    console.log(el);
     par.innerHTML = "i'm grey";
     el.stopPropagation();
 });
@@ -18,4 +23,20 @@ grey.addEventListener("click", el => {
 green.addEventListener("click", el => {
     console.log("i'm green");
     par.innerHTML = "i'm green";
+});
+
+btn.addEventListener("click", el => {
+    console.log("i'm btn");
+    console.log(el);
+    par.innerHTML = "i'm btn";
+    el.stopPropagation();
+});
+
+divDragAndDrop.addEventListener("mousemove", el => {
+    console.log("i'm divDragAndDrop");
+    console.log(el);
+    par.innerHTML = "i'm divDragAndDrop";
+    let newWidth = Math.max(10, el.clientX - divDragAndDrop.offsetLeft) ;
+    divDragAndDrop.style.width = newWidth + "px";
+    el.stopPropagation();
 });
